5acb108179a0eeb513f14a5f74b58b3806d30f5d
Aditya Bhavaraju <adityateja.b@gmail.com>
2018-04-20 02:19:50 +0530

    config changes

--- gazette/src/main/java/com/gazette/my/DBConfig.java
+++ gazette/src/main/java/com/gazette/my/DBConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+//Configuration class for mysql driver
 @Configuration
 public class DBConfig {
 
