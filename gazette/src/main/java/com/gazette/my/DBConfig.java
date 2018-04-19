c8de212404d2c88dbcdcaba1dddb0113eeb8798a
Aditya Bhavaraju <adityateja.b@gmail.com>
2018-04-20 02:17:05 +0530

    spring boot initialised
    spring boot initialised

---
+++ gazette/src/main/java/com/gazette/my/DBConfig.java
@@ -0,0 +1,22 @@
+package com.gazette.my;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DBConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/gazette");
+		dataSource.setUsername("root");
+		dataSource.setPassword("");
+		return dataSource;
+	}
+
+}
